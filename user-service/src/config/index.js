8f4d41997fbf5f7965e0ab615a29991ccc6cd024
Lucky <sashankswain6@gmail.com>
2026-04-06 00:01:28 +0530

    feat: Set default value for MAIL_SEND and add migration_lock.toml for inventory service

--- user-service/src/config/index.js
+++ user-service/src/config/index.js
@@ -23,7 +23,7 @@ const config = {
   REDIS_USER_TTL: Number(process.env.REDIS_USER_TTL || 86400),
 
 
-  MAIL_SEND: process.env.MAIL_SEND,
+  MAIL_SEND: process.env.MAIL_SEND || 'true',
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
 
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
